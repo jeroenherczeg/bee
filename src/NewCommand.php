5c25e42815c4005bdb3f82f3135ac47bf416d14f
Jeroen Herczeg <jeroen@herczeg.be>
2016-09-13 21:59:54 +0200

    Hello World

---
+++ src/NewCommand.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Jeroenherczeg\Bee;
+
+use ZipArchive;
+use RuntimeException;
+use GuzzleHttp\Client;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class NewCommand extends Command
+{
+    /**
+     * Configure the command options.
+     *
+     * @return void
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('generate')
+            ->setDescription('Generate a bee');
+    }
+
+    /**
+     * Execute the command.
+     *
+     * @param  InputInterface  $input
+     * @param  OutputInterface  $output
+     * @return void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('<info>Crafting application...</info>');
+
+
+        $output->writeln('<comment>Application ready! Build something amazing.</comment>');
+    }
+}
